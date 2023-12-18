c4a42cdae1790e75adcb7c49232e0d8bb392f16a
Anton Fagerberg <anton.fagerberg@husqvarnagroup.com>
2023-12-18 21:19:49 +0100

    DayX

---
+++ app/src/main/java/advent_of_code_2023/DayX.java
@@ -0,0 +1,13 @@
+package advent_of_code_2023;
+
+public class DayX {
+
+    static int part1(String[] input) {
+        return -1;
+    }
+
+    static int part2(String[] input) {
+        return -1;
+    }
+
+}
---
+++ app/src/test/java/advent_of_code_2023/DayXTest.java
@@ -0,0 +1,32 @@
+package advent_of_code_2023;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DayXTest {
+
+    String input = """
+            """;
+
+    @Test
+    void solve_part1_example() {
+        assertEquals(-1, DayX.part1(input.split("\n")));
+    }
+
+    @Test
+    void solve_part1() {
+        assertEquals(-1, DayX.part1(Util.INSTANCE.readFile("dayX")));
+    }
+
+    @Test
+    void solve_part2_example() {
+        assertEquals(-1, DayX.part2(input.split("\n")));
+    }
+
+    @Test
+    void solve_part2() {
+        assertEquals(-1, DayX.part2(Util.INSTANCE.readFile("dayX")));
+    }
+
+}
