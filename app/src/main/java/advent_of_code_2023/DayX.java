0dc1908f8f6a1f0ddfa074c9a5801d33f9b0def6
Anton Fagerberg <anton@antonfagerberg.com>
2023-12-19 08:16:20 +0100

    part1

--- app/src/main/java/advent_of_code_2023/DayX.java
+++ app/src/main/java/advent_of_code_2023/DayX.java
@@ -1,13 +1,96 @@
 package advent_of_code_2023;
 
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class DayX {
 
     static int part1(String[] input) {
-        return -1;
+        boolean isWorkflow = true;
+
+        List<Map<String, Integer>> parts = new ArrayList<>();
+        Map<String, List<Function<Map<String, Integer>, String>>> workflows = new HashMap<>();
+
+        for (var line : input) {
+            if (line.isBlank()) {
+                isWorkflow = false;
+            } else if (isWorkflow) {
+                var pieces = line.split("[{},]");
+
+                var name = pieces[0];
+                List<Function<Map<String, Integer>, String>> functions = new ArrayList<>();
+
+                for (int i = 1; i < pieces.length - 1; i++) {
+                    var ubp = pieces[i].split("[<>:]");
+
+                    final BiFunction<Integer, Integer, Boolean> comparator;
+                    if (pieces[i].contains(">")) {
+                        comparator = (a, b) -> a > b;
+                    } else if (pieces[i].contains("<")) {
+                        comparator = (a, b) -> a < b;
+                    } else {
+                        throw new IllegalStateException();
+                    }
+
+                    functions.add(
+                            map -> {
+                                if (comparator.apply(map.get(ubp[0]), Integer.parseInt(ubp[1]))) {
+                                    return ubp[2];
+                                }
+
+                                return null;
+                            }
+                    );
+                }
+
+                functions.add(m -> pieces[pieces.length - 1]);
+
+                workflows.put(name, functions);
+            } else {
+                var pieces = line.split("[{},=]");
+                var m = new HashMap<String, Integer>();
+                for (var i = 2; i < pieces.length; i += 2) {
+                    m.put(pieces[i - 1], Integer.parseInt(pieces[i]));
+                }
+                parts.add(m);
+            }
+        }
+
+        var accepted = new ArrayList<Map<String, Integer>>();
+
+        for (var part : parts) {
+            var w = workflows.get("in");
+            while (w != null) {
+                for (var f : w) {
+                    var r = f.apply(part);
+                    if (r != null) {
+                        if (r.equals("A")) {
+                            accepted.add(part);
+                            w = null;
+                            break;
+                        } else if (r.equals("R")) {
+                            w = null;
+                            break;
+                        } else {
+                            w = workflows.get(r);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return accepted.stream()
+                .map(Map::values)
+                .flatMap(Collection::stream)
+                .reduce(0, Integer::sum);
     }
 
-    static int part2(String[] input) {
-        return -1;
+    static long part2(String[] input) {
+
+
+        return -1L;
     }
 
 }
--- app/src/test/java/advent_of_code_2023/DayXTest.java
+++ app/src/test/java/advent_of_code_2023/DayXTest.java
@@ -7,21 +7,37 @@ import org.junit.jupiter.api.Test;
 public class DayXTest {
 
     String input = """
-            """;
+            px{a<2006:qkq,m>2090:A,rfg}
+            pv{a>1716:R,A}
+            lnx{m>1548:A,A}
+            rfg{s<537:gd,x>2440:R,A}
+            qs{s>3448:A,lnx}
+            qkq{x<1416:A,crn}
+            crn{x>2662:A,R}
+            in{s<1351:px,qqz}
+            qqz{s>2770:qs,m<1801:hdj,R}
+            gd{a>3333:R,R}
+            hdj{m>838:A,pv}
+                        
+            {x=787,m=2655,a=1222,s=2876}
+            {x=1679,m=44,a=2067,s=496}
+            {x=2036,m=264,a=79,s=2244}
+            {x=2461,m=1339,a=466,s=291}
+            {x=2127,m=1623,a=2188,s=1013}""";
 
     @Test
     void solve_part1_example() {
-        assertEquals(-1, DayX.part1(input.split("\n")));
+        assertEquals(19114, DayX.part1(input.split("\n")));
     }
 
     @Test
     void solve_part1() {
-        assertEquals(-1, DayX.part1(Util.INSTANCE.readFile("dayX")));
+        assertEquals(263678, DayX.part1(Util.INSTANCE.readFile("dayX")));
     }
 
     @Test
     void solve_part2_example() {
-        assertEquals(-1, DayX.part2(input.split("\n")));
+        assertEquals(167409079868000L, DayX.part2(input.split("\n")));
     }
 
     @Test
