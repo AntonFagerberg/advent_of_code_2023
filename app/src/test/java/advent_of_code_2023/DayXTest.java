976f1f6960e0d41e93110bdebf26d6d88d7637d8
Anton Fagerberg <anton@antonfagerberg.com>
2024-01-01 16:23:41 +0100

    cleaned up p1

--- app/src/test/java/advent_of_code_2023/DayXTest.java
+++ app/src/test/java/advent_of_code_2023/DayXTest.java
@@ -38,51 +38,8 @@ public class DayXTest {
 
     @Test
     void solve_part1() {
-        assertEquals(-1, DayX.part1(Util.INSTANCE.readFile("dayX")));
+        assertEquals(2246, DayX.part1(Util.INSTANCE.readFile("dayX")));
     }
 
-    @Test
-    void solve_part2_example() throws InterruptedException {
-        assertEquals(154, DayX.part2(input.split("\n")));
-    }
-
-    @Test
-    void solve_part2() throws InterruptedException {
-        // 4722 too low
-        // 5799 too low
-        assertEquals(-1, DayX.part2(Util.INSTANCE.readFile("dayX")));
-    }
-
-    @Test
-    void solve_part2_old() throws InterruptedException {
-        // 4722 too low
-        // 5799 too low
-        // 6223 not right
-        assertEquals(-1, DayX.part2_old(Util.INSTANCE.readFile("dayX")));
-    }
-
-    @Test
-    void solve_part3() throws InterruptedException {
-        // 4722 too low
-        // 5799 too low
-        // 6075?
-        assertEquals(-1, DayX.part3(Util.INSTANCE.readFile("dayX")));
-    }
-
-    @Test
-    void solve_part4() throws InterruptedException {
-        // 4722 too low
-        // 5799 too low
-        // 6075?
-        assertEquals(-1, DayX.part3_old(Util.INSTANCE.readFile("dayX")));
-    }
-
-    @Test
-    void solve_part4_ex() throws InterruptedException {
-        // 4722 too low
-        // 5799 too low
-        // 6575 -> nope
-        assertEquals(154, DayX.part3_old(input.split("\n")));
-    }
 
 }
